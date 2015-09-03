27506d412ac2a9a23ef08fd18e93f28f81417a00
morozovigor <perffectgame@gmail.com>
2015-09-03 12:18:55 +0300

    Sidebar

--- functions.php
+++ functions.php
@@ -71,7 +71,8 @@ function admin_menu(){
 //nav menus wordpress
 register_nav_menus( array(
     'header_menu' => 'Меню в шапке',
-    'footer_menu' => 'Меню в подвале'
+    'footer_menu' => 'Меню в подвале',
+    'sidebar' => 'Боковое меню'
 ) );
 
 // load script to admin
@@ -275,3 +276,25 @@ function trust_us_sc(){
     return $generate;
 }
 add_shortcode('trust_us', 'trust_us_sc');
+
+// custom menu example @ https://digwp.com/2011/11/html-formatting-custom-menus/
+function clean_custom_menus() {
+    $menu_name = 'sidebar'; // specify custom menu slug
+    if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
+        $menu = wp_get_nav_menu_object($locations[$menu_name]);
+        $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+        $menu_list = '<div class="sidebar">' ."\n";
+        $menu_list .= "\t\t\t\t". '<ul>' ."\n";
+        foreach ((array) $menu_items as $key => $menu_item) {
+            $title = $menu_item->title;
+            $url = $menu_item->url;
+            $menu_list .= "\t\t\t\t\t". '<a href="'. $url .'"><li>'. $title .'</li></a>' ."\n";
+        }
+        $menu_list .= "\t\t\t\t". '</ul>' ."\n";
+        $menu_list .= "\t\t\t". '</div>' ."\n";
+    } else {
+        // $menu_list = '<!-- no list defined -->';
+    }
+    echo $menu_list;
+}
--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,16 @@
-<div class="sidebar">
-	<ul>
-		<a href="#nowhere"><li>3PL</li></a>
-		<a href="#nowhere"><li>КД1</li></a>
-		<a href="#nowhere"><li>КД2</li></a>
-		<a href="#nowhere"><li>ОТВЕТСТВЕННОЕ ХРАНЕНИЕ</li></a>
-		<a href="#nowhere"><li>КРОСС-ДОКИНГ</li></a>
-		<a href="#nowhere"><li>МАРШРУТИЗАЦИЯ</li></a>
-		<a href="#nowhere"><li>ГРУЗОПЕРЕВОЗКИ</li></a>
-	</ul>
-</div>
+<!--<div class="sidebar">-->
+<!--	<ul>-->
+<!--		<a href="#nowhere"><li>3PL</li></a>-->
+<!--		<a href="#nowhere"><li>КД1</li></a>-->
+<!--		<a href="#nowhere"><li>КД2</li></a>-->
+<!--		<a href="#nowhere"><li>ОТВЕТСТВЕННОЕ ХРАНЕНИЕ</li></a>-->
+<!--		<a href="#nowhere"><li>КРОСС-ДОКИНГ</li></a>-->
+<!--		<a href="#nowhere"><li>МАРШРУТИЗАЦИЯ</li></a>-->
+<!--		<a href="#nowhere"><li>ГРУЗОПЕРЕВОЗКИ</li></a>-->
+<!--	</ul>-->
+<!--</div>-->
+<?php
+
+if (function_exists(clean_custom_menus())) clean_custom_menus();
+
+?>
