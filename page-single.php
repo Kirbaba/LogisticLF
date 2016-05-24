ab833dddfa7e8fe59beec61336494186c87a2319
Vladislav <v.kirbaba@gmail.com>
2016-05-24 15:58:56 +0300

    сингл

---
+++ page-single.php
@@ -0,0 +1,57 @@
+<? get_header() ?>
+	<!-- open .page-content -->
+		<section class="page-content page-presscenter">
+			<!-- open .contain -->
+			<div class="contain">
+				<h3>Пресс-центр</h3>
+				<!-- open .page-projects__categories -->
+				<ul class="page-projects__categories">
+					<li><a href="#">Все новости</a></li>
+					<li><a href="#">Про компанию</a></li>
+					<li><a href="#">Про ТРЦ</a></li>
+					<li><a href="#">СМИ о нас</a></li>
+				</ul>
+				<!-- close .page-projects__categories -->	
+
+				<!-- open .page-presscenter__content -->
+					<div class="page-single__content">
+						<!-- open .page-presscenter__item -->
+							<span class="page-presscenter__item_date">25 апреля 2016г.</span>
+							<h5>ТРЦ Ocean Plaza стал лауреатом Украины «Выбор потребителя»</h5>
+							<!-- open .page-presscenter__item_thumb -->
+							<div class="page-single__thumb"><img src="<?php bloginfo('template_directory'); ?>/img/Layer-42.png" alt="" /></div>
+							<!-- close .page-presscenter__item_thumb -->
+							<p>Компания «Логистик Л7» оказывает логистические услуги на территории Перми и Пермского края,
+							Республики Удмуртия, Екатеринбурга и Свердловской области и осуществляет грузоперевозки и 
+							экспедирование по всей территории России. </p>
+							<p>Ocean Plaza (GLA 69 000 м2, средняя посещаемость более 50 000 чел. в день), один из крупнейших торговых центров 
+							Украины, получил национальную премию Украины в сфере розничной торговли Retail Awards «Выбор потребителя» в 
+							номинации «Крупный торговый центр Киева». Премия Retail Awards призвана определять лидеров потребительских 
+							симпатий на рынке розничной торговли Украины. Победители в каждой категории определены по результатам онлайн-
+							голосования, проведенного международной исследовательской компанией TNS.</p>
+					</div>
+
+					<!-- open .page-single__other -->
+					<div class="page-single__other">
+						<!-- open .page-single__other_item -->
+						<h3>Другие новости</h3>
+						<div class="page-single__other_item">
+							<span class="page-presscenter__item_date">25 апреля 2016г.</span>
+							<a href="#" class="page-projects__item_title">ТРЦ Ocean Plaza стал лауреатом Украины «Выбор потребителя»</a>
+						</div>
+						<!-- close .page-single__other_item -->
+						
+						<div class="page-single__other_item">
+							<span class="page-presscenter__item_date">25 апреля 2016г.</span>
+							<a href="#" class="page-projects__item_title">ТРЦ Ocean Plaza стал лауреатом Украины «Выбор потребителя»</a>
+						</div>
+						<!-- close .page-single__other_item -->
+					</div>
+					<!-- close .page-single__other -->
+			</div>
+			<!-- close .contain -->
+			
+			
+		</section>
+	<!-- close .page-content -->
+<? get_footer() ?>
