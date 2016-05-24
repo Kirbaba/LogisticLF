311260665ddde466a11a71d734e413bf07896402
Vladislav <v.kirbaba@gmail.com>
2016-05-24 13:55:10 +0300

    Проекты

---
+++ page-projects.php
@@ -0,0 +1,62 @@
+<? get_header() ?>
+	<!-- open .page-content -->
+		<section class="page-content page-projects">
+			<!-- open .contain -->
+			<div class="contain">
+				<h3>Наши проекты</h3>
+				<!-- open .page-projects__categories -->
+				<ul class="page-projects__categories">
+					<li><a href="#">3PL</a></li>
+					<li><a href="#">Кросс-докинг</a></li>
+				</ul>
+				<!-- close .page-projects__categories -->
+				<!-- open .page-projects__item -->
+				<div class="page-projects__item">
+					<!-- open .page-projects__item_thumb -->
+					<div class="page-projects__item_thumb">
+						<img src="<?php bloginfo('template_directory'); ?>/img/Layer-7.jpg" alt="" />
+					</div>
+					<!-- close .page-projects__item_thumb -->
+					<!-- open .page-projects__item_text -->
+					<div class="page-projects__item_text">
+						<a href=""# class="page-projects__item_title">ТРЦ Ocean Plaza стал лауреатом Украины «Выбор потребителя»</a>
+						<p>В 2015 году совместно с компанией Кока-Кола нами был запущен 3PL-проект на территории Республики Удмуртия. </p>
+						<a href="#" class="proj__block--text--but">О ПРОЕКТЕ</a>
+					</div>
+					<!-- close .page-projects__item_text -->
+				</div>
+				<!-- close .page-projects__item -->
+				<!-- open .page-projects__item -->
+				<div class="page-projects__item">
+					<!-- open .page-projects__item_thumb -->
+					<div class="page-projects__item_thumb">
+						<img src="<?php bloginfo('template_directory'); ?>/img/Layer-7.jpg" alt="" />
+					</div>
+					<!-- close .page-projects__item_thumb -->
+					<!-- open .page-projects__item_text -->
+					<div class="page-projects__item_text">
+						<a href=""# class="page-projects__item_title">ТРЦ Ocean Plaza стал лауреатом Украины «Выбор потребителя»</a>
+						<p>В 2015 году совместно с компанией Кока-Кола нами был запущен 3PL-проект на территории Республики Удмуртия. </p>
+						<a href="#" class="proj__block--text--but">О ПРОЕКТЕ</a>
+					</div>
+					<!-- close .page-projects__item_text -->
+				</div>
+				<!-- close .page-projects__item -->
+
+				<!-- open .line -->
+				<div class="line">
+					<a href="#" class="page-projects_reload"></a>
+				</div>
+				<!-- close .line -->
+				
+				<!-- open .line -->
+				<div class="line">
+					<a href="#" class="page-projects_more">Посмотреть еще</a>
+				</div>
+				<!-- close .line -->
+				
+			</div>
+			<!-- close .contain -->
+		</section>
+	<!-- close .page-content -->
+<? get_footer() ?>
