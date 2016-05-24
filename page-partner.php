5ac37853c3806c501793dbd296167ce1bb2a907a
Vladislav <v.kirbaba@gmail.com>
2016-05-24 16:45:14 +0300

    Партнер

---
+++ page-partner.php
@@ -0,0 +1,90 @@
+<? get_header() ?>
+	<!-- open .page-content -->
+		<section class="page-content page-partner">
+			<!-- open .contain -->
+			<div class="contain">
+				<!-- open .page__nav -->
+				<nav class="page__nav">
+					<a href="#" class="page__nav_back">вернуться назад</a>
+				</nav>
+				<!-- close .page__nav -->
+				<!-- open .base_line -->
+				<div class="base_line">
+					<!-- open .page-partner__thumb -->
+					<div class="page-partner__thumb">
+						<img src="<?php bloginfo('template_directory'); ?>/img/Layer-42-1.png" alt="" />
+					</div>
+					<!-- close .page-partner__thumb -->
+					<!-- open .page-partner__preview -->
+					<div class="page-partner__preview">
+						<img src="<?php bloginfo('template_directory'); ?>/img/coca-cola.png" alt="" />
+						<strong>Самый крупный 3PL- проект компании Coca-Cola на территории России:</strong>
+						<p>Компания «Логистик Л7» оказывает логистические услуги
+						на территории Перми и Пермского края, Республики 
+						Удмуртия, Екатеринбурга и Свердловской области и 
+						осуществляет грузоперевозки и экспедирование по 
+						всей территории России. </p>
+					</div>
+					<!-- close .page-partner__preview -->	
+				</div>
+				<!-- close .base_line -->
+				
+				<!-- open .base_line -->
+				<div class="base_line">
+					<!-- open .page-partner__numbers -->
+					<div class="page-partner__numbers">
+						<!-- open .page-partner__numbers_item -->
+						<div class="page-partner__numbers_item">
+							<h2>3</h2>
+							<span>Региона</span>
+						</div>
+						<!-- close .page-partner__numbers_item -->
+						<!-- open .page-partner__numbers_item -->
+						<div class="page-partner__numbers_item">
+							<h2>28</h2>
+							<span>Единиц транспорта</span>
+						</div>
+						<!-- close .page-partner__numbers_item -->
+						<!-- open .page-partner__numbers_item -->
+						<div class="page-partner__numbers_item">
+							<h2>4</h2>
+							<span>Кросс-док площадки</span>
+						</div>
+						<!-- close .page-partner__numbers_item -->
+						<!-- open .page-partner__numbers_item -->
+						<div class="page-partner__numbers_item">
+							<h2>99,3%</h2>
+							<span>Показатель уровня сервиса</span>
+						</div>
+						<!-- close .page-partner__numbers_item -->
+						<!-- open .page-partner__numbers_item -->
+						<div class="page-partner__numbers_item">
+							<h2>15,6%</h2>
+							<span>Сокращение издержек</span>
+						</div>
+						<!-- close .page-partner__numbers_item -->
+					</div>
+					<!-- close .page-partner__numbers -->
+					<!-- open .page-partner__advantages -->
+					<ul class="page-partner__advantages">
+						<li>Уход от постоянных издержек</li>
+						<li>Фиксированная цена за доставку ед. продукции</li>
+						<li>Отсутствие повышения затрат на логистику ввиду сезонного колебания спроса</li>
+					</ul>
+					<!-- close .page-partner__advantages -->	
+					<!-- open .base_line -->
+					<div class="base_line">
+						<p>Компания «Логистик Л7» оказывает логистические услуги на территории Перми и Пермского края, Республики Удмуртия, Екатеринбурга и Свердловской области и осуществляет грузоперевозки и экспедирование по всей территории России. </p>	
+					</div>
+					<!-- close .base_line -->
+					
+				</div>
+				<!-- close .base_line -->
+				
+				
+			</div>
+			<!-- close .contain -->
+			
+		</section>
+	<!-- close .page-content -->
+<? get_footer() ?>
